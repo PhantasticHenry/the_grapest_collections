c298036f23919acd22cfb9b05c55589b0fcce43f
github username <github email address>
2020-03-06 19:36:11 -0800

    add get/post login request & login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,18 @@ class UsersController < ApplicationController
   end
 
   get '/login' do 
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    end
+    erb :"/users/login.html"
+  end
 
+  post '/login' do 
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
+    end
   end
 
   #READ
