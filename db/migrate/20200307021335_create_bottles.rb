6022df1104c440873e906d77bb72c5bb64279df1
github username <github email address>
2020-03-06 18:22:39 -0800

    add column type/nameas

--- db/migrate/20200307021335_create_bottles.rb
+++ db/migrate/20200307021335_create_bottles.rb
@@ -1,7 +1,10 @@
 class CreateBottles < ActiveRecord::Migration
   def change
     create_table :bottles do |t|
-
+      t.string :wine_type
+      t.integer :vintage
+      t.float  :price
+      t.integer :user_id
       t.timestamps null: false
     end
   end
