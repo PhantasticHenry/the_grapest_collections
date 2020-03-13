8c5862bb3ca0a2886200be228ba9fc1908a31de2
github username <github email address>
2020-03-12 17:07:50 -0700

    # encoding: UTF-8 # This file is auto-generated from the current state of the database. Instead # of editing this file, please use the migrations feature of Active Record to # incrementally modify your database, and then regenerate this schema definition. # # Note that this schema.rb definition is the authoritative source for your # database schema. If you need to create the application database on another # system, you should be using db:schema:load, not running all the migrations # from scratch. The latter is a flawed and unsustainable approach (the more migrations # you'll amass, the slower it'll run and the greater likelihood for issues). # # It's strongly recommended that you check this file into your version control system.
    ActiveRecord::Schema.define(version: 20200307021335) do
    
      create_table "bottles", force: :cascade do |t|
        t.string   "name"
        t.string   "grape"
        t.string   "style"
        t.integer  "vintage"
        t.float    "price"
        t.integer  "user_id"
        t.datetime "created_at", null: false
        t.datetime "updated_at", null: false
      end
    
      create_table "users", force: :cascade do |t|
        t.string   "username"
        t.string   "password_digest"
        t.datetime "created_at",      null: false
        t.datetime "updated_at",      null: false
      end
    
    end

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20200307021335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
